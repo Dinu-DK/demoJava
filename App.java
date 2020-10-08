bef3b3f87b2c7c7d5134c2ba1400b3fb0eddd849
Dinesh Kumar M <72552921+Dinu-DK@users.noreply.github.com>
2020-10-08 16:39:37 +0530

    Add files via upload

---
+++ App.java
@@ -0,0 +1,19 @@
+package com.hibernate.demoHibernate;
+
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        Alien saveDetails = new Alien();
+        saveDetails.setaId(101);
+        saveDetails.setaName("Dinesh");
+        saveDetails.setaColor("Yellow");
+        
+        Session
+    }
+}
